0732aae2eff3ae0e489cb12d06d50296ae38c891
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-02-07 23:45:26 +0600

    added again render postgres database

--- event_management/settings.py
+++ event_management/settings.py
@@ -87,26 +87,26 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 
 # postgreSQL database:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': 'amin@5078',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'amin@5078',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # render postgres db:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_manager_db_8s5u_user:kux6JI06zukKYeUYUJ0LCCttTduDGGxS@dpg-cui7jaaj1k6c73aq9nc0-a.oregon-postgres.render.com/event_manager_db_8s5u',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_manager_db_8s5u_user:kux6JI06zukKYeUYUJ0LCCttTduDGGxS@dpg-cui7jaaj1k6c73aq9nc0-a.oregon-postgres.render.com/event_manager_db_8s5u',
+        conn_max_age=600
+    )
+}
 
 # debug toolbar ip
 INTERNAL_IPS = [
